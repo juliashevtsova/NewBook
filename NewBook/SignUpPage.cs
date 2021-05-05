773c90f53e93e15f588a3a25599fc727ae8c012f
Julia Shevtsova <juliashevtsova96@gmail.com>
2021-05-05 13:43:24 +0300

    PageObjectSignUp

---
+++ NewBook/SignUpPage.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBook
+{
+    class SignUpPage
+    {
+        private readonly IWebDriver _webDriver;
+
+        private static readonly By _nameField = By.CssSelector("[name = first_name]");
+        private static readonly By _lastNameField = By.CssSelector("[name = last_name]");
+        private static readonly By _emailField = By.CssSelector("[name = email]");
+        private static readonly By _passwordField = By.CssSelector("[name = password]");
+        private static readonly By _passwordConfirmField = By.CssSelector("[name = password_confirm]");
+        private static readonly By _numberField = By.CssSelector("[name= phone_number]");
+        private static readonly By _registrationButton = By.CssSelector("[type = submit]");
+
+        public SignUpPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public SignUpPage OpenSignUpPage()
+        {
+            _webDriver.Navigate().GoToUrl("https://newbookmodels.com/join");
+            return this;
+        }
+
+        public SignUpPage SetName(string name)
+        {
+            _webDriver.FindElement(_nameField).SendKeys(name);
+            return this;
+        }
+        public SignUpPage SetLastName(string lastName)
+        {
+            _webDriver.FindElement(_lastNameField).SendKeys(lastName);
+            return this;
+        }
+        public SignUpPage SetEmail(string email)
+        {
+            _webDriver.FindElement(_emailField).SendKeys(email);
+            return this;
+        }
+        public SignUpPage SetPassword(string password)
+        {
+            _webDriver.FindElement(_passwordField).SendKeys(password);
+            return this;
+        }
+        public SignUpPage SetPasswordConfirm(string passwordConfirm)
+        {
+            _webDriver.FindElement(_passwordConfirmField).SendKeys(passwordConfirm);
+            return this;
+        }
+        public SignUpPage SetNumber(string number)
+        {
+            _webDriver.FindElement(_numberField).SendKeys(number);
+            return this;
+        }
+        public void ClickRegistrationButton()
+        {
+            _webDriver.FindElement(_registrationButton).Click();
+        }
+    }
+}
--- NewBook/UnitTest1.cs
+++ NewBook/UnitTest1.cs
@@ -7,7 +7,7 @@ using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager.Helpers;
 
 
-namespace NewBookModels
+namespace NewBook
 {
     public class Registration
     {
@@ -17,55 +17,26 @@ namespace NewBookModels
         {
             new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
             _webDriver = new ChromeDriver();
-
-            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1000);
             _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
         }
 
         [Test]
-        public void InputValidDate()
+        public void InputValidFirstDate()
         {
-            _webDriver.Navigate().GoToUrl("https://newbookmodels.com/join");
-
-
-            var name = _webDriver.FindElement(By.CssSelector("[name = first_name]"));
-            name.SendKeys("Julia");
-            var lastName = _webDriver.FindElement(By.CssSelector("[name = last_name]"));
-            lastName.SendKeys("Shevtsova");
-            var email = _webDriver.FindElement(By.CssSelector("[name = 'email']"));
-            email.SendKeys("assssaio@gmail.com");
-            var password = _webDriver.FindElement(By.CssSelector("[name = 'password']"));
-            password.SendKeys("julia28091999A!");
-            var repeatPassword = _webDriver.FindElement(By.CssSelector("[name = 'password_confirm']"));
-            repeatPassword.SendKeys("julia28091999A!");
-            var number = _webDriver.FindElement(By.CssSelector("[name= 'phone_number']"));
-            number.SendKeys("3456786542");
-            _webDriver.FindElement(By.CssSelector("[type = 'submit']")).Click();
-
-            System.Threading.Thread.Sleep(4000);
-
-            var companyName = _webDriver.FindElement(By.CssSelector("[name = 'company_name']"));
-            companyName.SendKeys("Julia");
-            var companyWebsite = _webDriver.FindElement(By.CssSelector("[name = 'company_website']"));
-            companyWebsite.SendKeys("julia.com");
-            var address = _webDriver.FindElement(By.CssSelector("[name = 'location']"));
-            address.SendKeys("1");
-           
-            System.Threading.Thread.Sleep(2000);
-            _webDriver.FindElements(By.CssSelector("[class^='pac-matched']"))[0].Click();
-
-
-            _webDriver.FindElement(By.CssSelector("[name='industry']")).Click();
-            _webDriver.FindElements(By.CssSelector("[role='option']"))[0].Click();
-            System.Threading.Thread.Sleep(4000);
-            _webDriver.FindElement(By.CssSelector("[type = submit]")).Click();
-
-            System.Threading.Thread.Sleep(10000);
+            var signUpPage = new SignUpPage(_webDriver);
+            signUpPage.OpenSignUpPage()
+                .SetName("Julia")
+                .SetLastName("Shevtsova")
+                .SetEmail("juliashevtsova26@gmail.com")
+                .SetPassword("12345678qQ!")
+                .SetPasswordConfirm("12345678qQ!")
+                .SetNumber("1234567890")
+                .ClickRegistrationButton();
 
             var result = _webDriver.Url;
 
-            Assert.AreEqual("https://newbookmodels.com/explore", result);
-
+            Assert.AreEqual("https://newbookmodels.com/join/company", result);
         }
     }
     public class Login
